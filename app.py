cca7ce8ad50bf0569bb1a985f56347a2e8dbcd42
Winny Dwita <winnysumbayak410@gmail.com>
2026-05-12 22:20:17 +0700

    Refactor app.py for improved model loading and response

--- app.py
+++ app.py
@@ -304,5 +304,8 @@ def predict():
 # =========================
 # RUN APP
 # =========================
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
